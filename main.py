d0e7e8ee1f91a9e815bdff8ea44d0c33b0ac47a1
Halasi Peter <halasi.peter@protonmail.com>
2025-06-25 15:11:32 +0200

    Do drawing with variables.

--- main.py
+++ main.py
@@ -19,33 +19,37 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PyDrum")
 label_font = pygame.font.Font('freesansbold.ttf', 32)
 
+boxes = []
 fps = 60
 beats = 8
-instruments = ["Kick", "Snare", "Hi-Hat", "Bass Drum", "Kick"]
+instruments = ["Kick", "Snare", "Hi-Hat", "Bass Drum", "Crash"]
 timer = pygame.time.Clock()
 
 def draw_grid():
-    left_box = pygame.draw.rect(screen, white, [0, 0, 250, HEIGHT-250], 5)
-    bottom_box = pygame.draw.rect(screen, white, [0, HEIGHT-250, WIDTH, 250], 5)
-    boxes = []
-    colors = [dark_grey, white, light_grey]
-    for idx,sound in enumerate(["Kick", "Snare", "Hi-Hat", "Bass Drum", "Kick"]):
-        # box = pygame.draw.rect(screen, colors.pop(0), [0, 0, 200, 200], 5)
-        # boxes.append(box)
-        # colors.append(light_grey)
-        text = label_font.render(sound, True, green)
-        screen.blit(text, (20, idx*100+30))
-        pygame.draw.rect(screen, light_grey, [0, idx*100+10, 300, 75], 5)
-        # colors.append(grey)
-    
-    for j in range(len(instruments)):
+    lbox_width = 250    
+    bbox_height = 250
 
-        text = label_font.render(instruments[j], True, green)
-        screen.blit(text, (20, j*100+30))
-        pygame.draw.rect(screen, grey, [0, j*100+10, 250, 75], 5)
+    lbox_height = HEIGHT - bbox_height
+    bbox_width  = WIDTH
+
+    left_box = pygame.draw.rect(screen, white, [0, 0, lbox_width, lbox_height], 5)
+    bottom_box = pygame.draw.rect(screen, white, [0, lbox_height, WIDTH, bbox_height], 5)
+    boxes = []
+    colors = [ white, red, blue, green, dark_grey, light_grey ]
+
+    row_hight = lbox_height // len(instruments)
+    col_width = (WIDTH - lbox_width) // beats
+
+    for idx,sound in enumerate(instruments):
+        text = label_font.render(instruments[idx], True, green)
+        screen.blit(text, (30, idx*row_hight+30))
+        pygame.draw.line(screen, white, (0, idx*row_hight), (250, idx*row_hight), 5)
 
         for i in range(beats):
-            pygame.draw.rect(screen, grey, [i*150+250, j*100+10, 140, 75], 5)
+            rect = pygame.draw.rect(screen, light_grey, [lbox_width+i*col_width, idx*row_hight, col_width, row_hight], 5)
+            boxes.append((rect,(idx, i)))
+
+    return boxes
 
 
 
@@ -54,7 +58,7 @@ while running:
     timer.tick(fps)
     screen.fill(black)
 
-    draw_grid()
+    boxes = draw_grid()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
