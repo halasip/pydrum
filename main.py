7d241fe38f22483f0eba6ac947d649719d7b137c
Halasi Peter <halasi.peter@protonmail.com>
2025-06-25 17:38:48 +0200

    Adding play/pause button. Adding bpm changing option.

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ light_grey = (200, 200, 200)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PyDrum")
 label_font = pygame.font.Font('freesansbold.ttf', 32)
+medium_font = pygame.font.Font('freesansbold.ttf', 24)
 
 playing = True
 active_length = 0
@@ -41,6 +42,8 @@ for file in sorted(os.listdir(f'sounds/kit{kitidx}')):
     sound_name = file.split('.')[0]
     sounds.append(mixer.Sound(os.path.join(f'sounds/kit{kitidx}', file)))
     instruments.append(sound_name.title())
+
+pygame.mixer.set_num_channels(len(sounds)*3)
 
 clicked_boxes = [[ -1 for _ in range(beats)] for _ in range(len(instruments))]
 
@@ -83,6 +86,11 @@ def draw_grid():
     # -5 and +10 to make the active beat rectangle wider and more visible
     active_beat_rect = pygame.draw.rect(screen, green, [lbox_width + active_beat * col_width -5, 0, col_width +10, lbox_height], 10, 5)
 
+    # lower menu buttons
+    
+
+
+
     return boxes
 
 
@@ -97,11 +105,46 @@ while running:
         play_sound()
         beat_changed = False
 
+
+
+    play_box = pygame.draw.rect(screen, dark_grey, [50, HEIGHT-150, 200, 100], 0, 5)
+    if playing:
+        text = label_font.render("Play", True, white)
+        if beat_changed:
+            play_sound()
+            beat_changed = False
+    else:
+        text = label_font.render("Pause", True, white)
+    screen.blit(text, (75, HEIGHT-120))
+
+    bpm_box = pygame.draw.rect(screen, dark_grey, [300, HEIGHT-150, 200, 100], 5, 5)
+    bpm_text = medium_font.render(f"{bpm} bpm", True, white)
+    screen.blit(bpm_text, (325, HEIGHT-130))
+    bpm_change_rect = []
+    for i in range(4):
+        bpm_change_rect.append(pygame.draw.rect(screen, green, [300+i*50, HEIGHT-100, 50, 50], 5, 5))
+        match(i):
+            case(0): screen.blit(medium_font.render("<<", True, white), (310+i*50, HEIGHT-90))
+            case(1): screen.blit(medium_font.render("<",  True, white), (320+i*50, HEIGHT-90))
+            case(2): screen.blit(medium_font.render(">",  True, white), (320+i*50, HEIGHT-90))
+            case(3): screen.blit(medium_font.render(">>", True, white), (310+i*50, HEIGHT-90))
+
+
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
+            for i in range(len(bpm_change_rect)):
+                if bpm_change_rect[i].collidepoint(event.pos):
+                    match(i):
+                        case(0): bpm -= 5
+                        case(1): bpm -= 1
+                        case(2): bpm += 1
+                        case(3): bpm += 5
+                        
             for i in range(len(boxes)):
                 if boxes[i][0].collidepoint(event.pos):
                     coords = boxes[i][1]
@@ -109,6 +152,18 @@ while running:
                     print(f"Clicked on {instruments[coords[0]]} at beat {coords[1]}")
                     # Here you can add sound playing logic
                     pygame.draw.rect(screen, red, boxes[i][0])
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            if play_box.collidepoint(event.pos):
+                playing = not playing
+                if playing:
+                    active_length = 0
+                    active_beat = 0
+                    beat_changed = True
+                else:
+                    active_length = 0
+
+
 
     beat_length = fps * 60 // bpm
     if playing:
